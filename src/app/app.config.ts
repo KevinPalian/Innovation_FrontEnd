53069ba0f5c5d671d63a2e65aa597a9f2525215e
daniherrera <u20201e786@upc.edu.pe>
2025-07-04 23:42:17 -0500

    prueben

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { HTTP_INTERCEPTORS, provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+import { JwtModule } from '@auth0/angular-jwt';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()),
+    importProvidersFrom(
+      JwtModule.forRoot({
+        config: {
+          tokenGetter: tokenGetter,
+          allowedDomains: ['localhost:8080'],
+          disallowedRoutes: ['http://localhost:8080/login/forget'],
+        },
+      })
+    ),
+    provideAnimationsAsync(),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }, provideCharts(withDefaultRegisterables()), provideCharts(withDefaultRegisterables())]
 };
+
+export function tokenGetter() {
+  return (typeof window !== 'undefined') ? sessionStorage.getItem('token') : null;
+}
